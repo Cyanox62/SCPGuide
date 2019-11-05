8d45eb468d90bf4ee44e44f44c2eff673680af19
Cyanox62 <Infernogaming621@gmail.com>
2019-11-05 15:05:14 -0500

    Add project files.

---
+++ SCPGuide/CommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Smod2;
+using Smod2.Commands;
+
+namespace SCPGuide
+{
+	class CommandHandler : ICommandHandler
+	{
+		public string GetCommandDescription()
+		{
+			return "";
+		}
+
+		public string GetUsage()
+		{
+			return "";
+		}
+
+		public string[] OnCall(ICommandSender sender, string[] args)
+		{
+			return new[] { "" };
+		}
+	}
+}
---
+++ SCPGuide/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SCPGuide")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SCPGuide")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ef825145-eb03-405f-bdd8-3235d174da14")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
